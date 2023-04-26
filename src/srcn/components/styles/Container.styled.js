a8905838b98520599f392a91a230a45632693818
Pirate-Emperor <piratekingrahul@gmail.com>
2023-04-26 15:24:14 +0530

    Updates

--- src/srcn/components/styles/Container.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledContainer = styled.div `
-  width: 100%;
-  max-width: ${({ theme }) => theme.maxWidth};
-  display: flex;
-  margin: auto;
-`;
